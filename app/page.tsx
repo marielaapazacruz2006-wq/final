f81b03e84af0699c07cba344755c72111dd91582
marielaapazacruz2006-wq <marielaapazacruz2006@gmail.com>
2026-06-13 10:42:28 -0400

    pantalla principal realizada

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import ExerciseList from "@/components/ExerciseList";
+
+export default function Home() {
+  return (
+    <main className="screen">
+      <Header />
+      <ExerciseList />
+    </main>
+  );
+}
