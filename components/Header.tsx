cbcbed009e9e1a57631a575c33d1e7e49b06743b
marielaapazacruz2006-wq <marielaapazacruz2006@gmail.com>
2026-06-13 10:42:01 -0400

    componente Header realizado

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <section className="header">
+      <span className="info">info</span>
+
+      <div className="headerRow">
+        <div>
+          <h1>Exercícios</h1>
+          <p>item</p>
+        </div>
+
+        <span className="count">4</span>
+      </div>
+    </section>
+  );
+}
